b558f06611d349935c54afef404160e6605de229
RaeJansen <rapajansen14@gmail.com>
2025-12-30 14:02:38 -0700

    url displaying

--- pinterest-board-export.sh
+++ pinterest-board-export.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 # Required parameters:
 # @raycast.schemaVersion 1
@@ -12,11 +12,37 @@
 # @raycast.description export images on a pinterest board
 # @raycast.author Rae Jansen
 
+set -euo pipefail
+
 # grab the browser you've opened raycast over
 FRONT_APP="$(osascript -e 'tell application "System Events" to get name of first application process whose frontmost is true')"
-    #### echo "Frontmost app: $FRONT_APP"
+    ## comment out everything after & uncomment this echo to see how the browser of your choice displays
+    ## echo "Frontmost app: $FRONT_APP"
 
-# grab url of the active window (change to the browser of your choice)
-URL_ARC="$(osascript -e 'tell application "Arc" to get URL of active tab of front window')"
-    echo "URL: $URL_ARC"
+URL=""
+
+# based on which app is in the front, grab url of the active window (add any of your choice, these are the two I use)
+if [[ "$FRONT_APP" == "Arc" ]]; then
+  URL="$(osascript \
+    -e 'try' \
+    -e 'tell application "Arc"' \
+    -e 'tell front window to tell active tab to get URL' \
+    -e 'end tell' \
+    -e 'on error' \
+    -e 'return ""' \
+    -e 'end try')"
+
+
+elif [[ "$FRONT_APP" == "Safari" ]]; then
+  URL="$(osascript -e 'try
+    tell application "Safari" to get URL of front document
+  on error
+    return ""
+  end try')"
+
+fi
+
+echo ""
+echo "URL: "
+echo "$URL"
     
